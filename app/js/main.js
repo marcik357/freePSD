bedc4eeffaf0542359d3108debe849eb83c10a8a
marcik357 <marcik357@gmail.com>
2019-03-24 23:47:32 +0200

    made a lot of things and start mobile

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,22 @@
 $(function () {
-      $('.main__slider').slick({
-        prevArrow: '<button type="button" class="slick-prev slick-btn"></button>',
-        nextArrow: '<button type="button" class="slick-next slick-btn"></button>',
-        slidesToScroll: 1,
-      });
+
+  $('.main__slider').slick({
+    prevArrow: '<button type="button" class="slick-prev slick-btn"></button>',
+    nextArrow: '<button type="button" class="slick-next slick-btn"></button>',
+    slidesToScroll: 1,
+    infinite: false,
+  });
+
+  $('.news__slider').slick({
+    prevArrow: '<button type="button" class="slick-prev slick-btn"></button>',
+    nextArrow: '<button type="button" class="slick-next slick-btn"></button>',
+    infinite: true,
+    slidesToShow: 1,
+    variableWidth: true
+  });
+
+  $('.menu__btn, .menu ul li a').click(function () {
+    $('.menu ul').slideToggle();
+  });
+  
 });
