18c4b7fc2f80a4dda48efec365c30eb98ecc4667
marcik357 <marcik357@gmail.com>
2019-03-25 23:54:22 +0200

    finish...

--- app/js/main.js
+++ app/js/main.js
@@ -10,13 +10,25 @@ $(function () {
   $('.news__slider').slick({
     prevArrow: '<button type="button" class="slick-prev slick-btn"></button>',
     nextArrow: '<button type="button" class="slick-next slick-btn"></button>',
-    infinite: true,
     slidesToShow: 1,
     variableWidth: true
   });
 
   $('.menu__btn, .menu ul li a').click(function () {
     $('.menu ul').slideToggle();
+  });
+
+  $(".menu").on("click", "a", function (event) {
+    //отменяем стандартную обработку нажатия по ссылке
+    event.preventDefault();
+    //забираем идентификатор бока с атрибута href
+    var id = $(this).attr('href'),
+      //узнаем высоту от начала страницы до блока на который ссылается якорь
+      top = $(id).offset().top;
+    //анимируем переход на расстояние - top за 1500 мс
+    $('body,html').animate({
+      scrollTop: top
+    }, 1000);
   });
   
 });
