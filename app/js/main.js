fb78472dafc3e6fc08087c6bc8d84d9f5bc300e1
marcik357 <marcik357@gmail.com>
2019-03-28 17:55:58 +0200

    changed news

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,17 @@
 $(function () {
 
-  $('.main__slider').slick({
+  $('.main__slider, .news__slider').slick({
     prevArrow: '<button type="button" class="slick-prev slick-btn"></button>',
     nextArrow: '<button type="button" class="slick-next slick-btn"></button>',
     slidesToScroll: 1,
-    infinite: false,
   });
 
-  $('.news__slider').slick({
-    prevArrow: '<button type="button" class="slick-prev slick-btn"></button>',
-    nextArrow: '<button type="button" class="slick-next slick-btn"></button>',
-    slidesToShow: 1,
-    variableWidth: true
-  });
+  // $('.news__slider').slick({
+  //   prevArrow: '<button type="button" class="slick-prev slick-btn"></button>',
+  //   nextArrow: '<button type="button" class="slick-next slick-btn"></button>',
+  //   slidesToShow: 1,
+  //   variableWidth: true
+  // });
 
   $('.menu__btn, .menu ul li a').click(function () {
     $('.menu ul').slideToggle();
